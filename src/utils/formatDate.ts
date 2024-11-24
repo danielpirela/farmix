d1de09295ee10515132909364a95996c00499fcf
daniel pirela <danielapirelac@gmail.com>
2024-11-24 17:48:02 -0400

    add utils

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,14 @@
+export function formatDate(isoDate: string): string { // Tipado agregado
+  const date = new Date(isoDate)
+
+  // Obtener componentes de la fecha
+  const day = String(date.getDate()).padStart(2, '0')
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const year = String(date.getFullYear()).slice(-2)
+  const hours = String(date.getHours() % 12 || 12).padStart(2, '0') // Ajuste para AM/PM
+  const minutes = String(date.getMinutes()).padStart(2, '0')
+  const ampm = date.getHours() >= 12 ? 'PM' : 'AM' // Determinar AM o PM
+
+  // Formatear como dd/mm/yy h:m AM/PM
+  return `${day}/${month}/${year} ${hours}:${minutes} ${ampm}`
+}
--- src/utils/validations.tsx
+++ src/utils/validations.tsx
@@ -1,38 +1,48 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 export const USER_FORM_FIELDS = {
-  firstName: "firstName",
-  lastName: "lastName",
-  idDocument: "idDocument",
-  phone: "phone",
-  address: "address",
-  hireDate: "hireDate",
-} as const;
+  firstName: 'firstName',
+  lastName: 'lastName',
+  idDocument: 'idDocument',
+  phone: 'phone',
+  address: 'address',
+  hireDate: 'hireDate'
+} as const
+
+export const ACTIVITY_FORM_FIELDS = {
+  type: 'type',
+  descrption: 'description'
+} as const
 
 const REGEX_DATE = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/
 
-export const userSchema = z
-  .object({
-    [USER_FORM_FIELDS.firstName]: z.string().min(1, "Nombre es obligatorio"),
-    [USER_FORM_FIELDS.lastName]: z.string().min(1, "Apellido es obligatorio"),
-    [USER_FORM_FIELDS.idDocument]: z.string().min(1, "Cédula es obligatoria"),
-    [USER_FORM_FIELDS.phone]: z
-      .string()
-      .min(14, "Teléfono es obligatorio"),
-    [USER_FORM_FIELDS.address]: z
-      .string()
-      .min(16, "Dirección debe tener al menos 16 caracteres"),
-    [USER_FORM_FIELDS.hireDate]: z
-      .string()
-      .regex(
-        REGEX_DATE,
-        "La fecha debe estar en formato dd/mm/yyyy"
+export const activitySchema = z.object({
+  [ACTIVITY_FORM_FIELDS.descrption]: z
+    .string()
+    .min(5, 'Escriba una descripcion'),
+  [ACTIVITY_FORM_FIELDS.type]: z.string().min(4, 'Seleccione un tipo de tarea')
+})
+export const userSchema = z.object({
+  [USER_FORM_FIELDS.firstName]: z.string().min(1, 'Nombre es obligatorio'),
+  [USER_FORM_FIELDS.lastName]: z.string().min(1, 'Apellido es obligatorio'),
+  [USER_FORM_FIELDS.idDocument]: z.string().min(1, 'Cédula es obligatoria'),
+  [USER_FORM_FIELDS.phone]: z.string().min(14, 'Teléfono es obligatorio'),
+  [USER_FORM_FIELDS.address]: z
+    .string()
+    .min(16, 'Dirección debe tener al menos 16 caracteres'),
+  [USER_FORM_FIELDS.hireDate]: z
+    .string()
+    .regex(REGEX_DATE, 'La fecha debe estar en formato dd/mm/yyyy')
+    .refine((date) => {
+      const [day, month, year] = date.split('/').map(Number)
+      const dateObject = new Date(year, month - 1, day) // Año, mes (0-11), día
+      return (
+        dateObject.getDate() === day &&
+        dateObject.getMonth() === month - 1 &&
+        dateObject.getFullYear() === year
       )
-      .refine((date) => {
-        const [day, month, year] = date.split("/").map(Number);
-        const dateObject = new Date(year, month - 1, day); // Año, mes (0-11), día
-        return dateObject.getDate() === day && dateObject.getMonth() === month - 1 && dateObject.getFullYear() === year;
-      }, "La fecha proporcionada no es válida")
-  });
+    }, 'La fecha proporcionada no es válida')
+})
 
-export type User = z.infer<typeof userSchema>;
+export type User = z.infer<typeof userSchema>
+export type Activity = z.infer<typeof activitySchema>
