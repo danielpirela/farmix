bd9e57a50d1da1f70d419bb393c3580f2898ef49
daniel pirela <danielapirelac@gmail.com>
2024-11-24 17:53:41 -0400

    add providers

--- src/components/context/Provider.tsx
+++ src/components/context/Provider.tsx
@@ -1,43 +1,43 @@
 'use client'
-import React, { createContext, useState, useContext, ReactNode } from 'react';
-import { Employee } from '../../types'
+import React, { createContext, useState, useContext, ReactNode } from 'react'
+import { Employee } from '../../models/types'
 
 type MyContextType = {
-  profile: Employee;
-  setProfile: (employee: Employee) => void;
-};
+  profile: Employee
+  setProfile: (employee: Employee) => void
+}
 
 // Estado inicial para el objeto `profile`
 const initialProfile: Employee = {
-  employee_id: "",
-  email: "",
-  first_name: "",
-  last_name: "",
-  id_document: "",
-  phone: "",
-  address: "",
-  hire_date: "",
+  employee_id: '',
+  email: '',
+  first_name: '',
+  last_name: '',
+  id_document: '',
+  phone: '',
+  address: '',
+  hire_date: '',
   salary: 0,
-  role_id: "",
-};
+  role_id: ''
+}
 
 // Creamos el contexto con el tipo especificado y valor inicial
-const MyContext = createContext<MyContextType | undefined>(undefined);
+const MyContext = createContext<MyContextType | undefined>(undefined)
 
 // Creamos el proveedor
 export const MyProvider = ({ children }: { children: ReactNode }) => {
-  const [profile, setProfile] = useState<Employee>(initialProfile);
+  const [profile, setProfile] = useState<Employee>(initialProfile)
 
   return (
     <MyContext.Provider value={{ profile, setProfile }}>
       {children}
     </MyContext.Provider>
-  );
-};
+  )
+}
 
 // Custom hook para usar el contexto
 export const useMyContext = () => {
-  const context = useContext(MyContext);
-  if (!context) throw new Error("useMyContext debe usarse dentro de MyProvider");
-  return context;
-};
+  const context = useContext(MyContext)
+  if (!context) throw new Error('useMyContext debe usarse dentro de MyProvider')
+  return context
+}
